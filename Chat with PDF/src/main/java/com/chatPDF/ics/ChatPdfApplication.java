2c4d83254c9a6f25137cc0073024718894109c8d
Nusrat Jahan <nusrat.ruet.ece15@gmail.com>
2025-10-08 16:19:41 +0600

    Add files via upload

---
+++ Chat with PDF/src/main/java/com/chatPDF/ics/ChatPdfApplication.java
@@ -0,0 +1,13 @@
+package com.chatPDF.ics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatPdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatPdfApplication.class, args);
+	}
+
+}
---
+++ Chat with PDF/src/test/java/com/chatPDF/ics/ChatPdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatPDF.ics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatPdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
